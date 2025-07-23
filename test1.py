e09e6f6655a1b418eef88c9982f4bbd147536754
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-23 14:26:57 +0300

    added a simple comment

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
 print ("hello world")
+
+for i in range (5):
+    print (i, " hello world")
+
+A = ["nick", "alex", "thanos", "makis"]
+
+for name in A:
+    print(name)
+
+list.sort(A)
+for name in A:
+    print(name)
+
+# hello world
