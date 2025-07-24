cf7aad31b63d980cb03c9be1ea543d5b9eed88ac
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-24 19:19:01 +0300

    Separated main from the encyption

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from cryptography.fernet import Fernet
-
+from encryption import encrypt
 from create_key import create_key
 
 
 plaintext= input("Type the phrase you want to encrypt: ")
 print("You typed", plaintext)
 
-#generate the encryption key if it doesnt exist, this checks if it already exists
+encrypt(plaintext)
 
-create_key()
 
-#load the key
-with open("filekey.key", "rb") as key_file:
-    key = key_file.read()
-f=Fernet(key)
-
-#encrypt the plaintext
-byte_plaintext = plaintext.encode()
-cyphertext = f.encrypt(byte_plaintext)
-
-print("the encrypted phrase is: ", cyphertext) 
