9d03207e244bdf531e510ec53e6f968c640d67ce
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-25 17:30:11 +0300

    Added an array of weak passwords i want to encrypt

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from encryption import encrypt
 from encryption import decrypt
 from create_key import create_key
 
+#an array of common and weak passwords
+
+A=["123456","password","123456789","12345678","12345","qwerty","abc123","football","monkey","letmein","admin","welcome","login","princess","dragon","passw0rd","master","hello","freedom","whatever","qazwsx","trustno1","654321","123123"]
 
 plaintext= input("Type the phrase you want to encrypt: ")
 print("You typed", plaintext)
