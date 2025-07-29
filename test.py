f006483e7636e91084f46422f6d0f874a3a4f6e0
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-29 20:05:03 +0300

    fixed pygiglet

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from encryption import decrypt
 from create_key import create_key
 from encryption_list import encrypt_list
 from encryption_list import decrypt_list
+import pyfiglet
 import os
-
 # this is a mock test to check if encryption is running fine before writing to the file
 
 A=["123456","password","123456789","12345678","12345","qwerty","abc123","football","monkey","letmein","admin","welcome","login","princess","dragon","passw0rd","master","hello","freedom","whatever","qazwsx","trustno1","654321","123123"]
@@ -17,3 +17,8 @@ C=encrypt_list(A)
 print(C)
 P = decrypt_list(C)
 print(P)
+
+# this plays with the pyfiglet lib 
+
+ascii_banner = pyfiglet.figlet_format("Hello, World!")
+print(ascii_banner)
