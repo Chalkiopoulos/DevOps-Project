f84324438ed19344859d09b3f5d011447261020c
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-29 20:00:48 +0300

    added pyfiglet to play with and test caching

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from create_key import create_key
 from encryption_list import encrypt_list
 from encryption_list import decrypt_list
 import os
+import pyfiglet
+
 
 A=["123456","password","123456789","12345678","12345","qwerty","abc123","football","monkey","letmein","admin","welcome","login","princess","dragon","passw0rd","master","hello","freedom","whatever","qazwsx","trustno1","654321","123123"]
 
@@ -16,3 +18,8 @@ with open('encrypted_passwords.txt', 'w') as f:
     for cypher in C:
         cyphertext = cypher.decode()
         f.write(f"{cyphertext}\n")
+
+        # this plays with the pyfiglet lib 
+
+ascii_banner = pyfiglet.figlet_format("Hello, World!")
+print(ascii_banner)
