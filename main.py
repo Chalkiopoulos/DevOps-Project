ecbd201746c32b5673f58086f4774dd56565a020
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-29 12:44:42 +0300

    changed main to write passwords in a file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from encryption import decrypt
 from create_key import create_key
 from encryption_list import encrypt_list
 from encryption_list import decrypt_list
+import os
 
 A=["123456","password","123456789","12345678","12345","qwerty","abc123","football","monkey","letmein","admin","welcome","login","princess","dragon","passw0rd","master","hello","freedom","whatever","qazwsx","trustno1","654321","123123"]
 
@@ -14,3 +15,8 @@ print(C)
 P=decrypt_list(C)
 print(P)
 
+
+with open('encrypted_passwords.txt', 'w') as f:
+    for cypher in C:
+        cyphertext = cypher.decode()
+        f.write(f"{cyphertext}\n")
