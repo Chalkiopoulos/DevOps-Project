8b309e982f7f2246c3f7c6bfe55060478b1a6991
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-29 16:15:01 +0300

    created a mock test and main now only writes on file

--- test.py
+++ test.py
@@ -6,13 +6,14 @@ from encryption_list import encrypt_list
 from encryption_list import decrypt_list
 import os
 
+# this is a mock test to check if encryption is running fine before writing to the file
+
 A=["123456","password","123456789","12345678","12345","qwerty","abc123","football","monkey","letmein","admin","welcome","login","princess","dragon","passw0rd","master","hello","freedom","whatever","qazwsx","trustno1","654321","123123"]
+C=[]
+P=[]
 
+print(A)
 C=encrypt_list(A)
-
-# write the encrypted passwords in a file
-
-with open('encrypted_passwords.txt', 'w') as f:
-    for cypher in C:
-        cyphertext = cypher.decode()
-        f.write(f"{cyphertext}\n")
+print(C)
+P = decrypt_list(C)
+print(P)
