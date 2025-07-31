e153de93e96dbcd5e6efa35f41d89ec196f1fad5
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-07-31 21:34:59 +0300

    push workflow recognizes the new encryption file names

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ from encryption import decrypt
 from create_key import create_key
 from encryption_list import encrypt_list
 from encryption_list import decrypt_list
-import os
+import platform
 import pyfiglet
 
+os_name = platform.system() #gets the name of the operating system
+encryption_file_name = f"encrypted_passwords_{os_name}.txt"
 with open("passwords.txt", "r") as f:       #reads the passwords
     passwords = [line.strip() for line in f]
 
 C=encrypt_list(passwords)
 # write the encrypted passwords in a file
 
-with open('encrypted_passwords.txt', 'w') as f:
+with open(encryption_file_name, 'w') as f:
     for cypher in C:
         cyphertext = cypher.decode()
         f.write(f"{cyphertext}\n")
 
 # this plays with the pyfiglet lib 
 
-ascii_banner = pyfiglet.figlet_format("CI/CD IS COOL")
+ascii_banner = pyfiglet.figlet_format("GitHub actions are COOL")
 print(ascii_banner)
