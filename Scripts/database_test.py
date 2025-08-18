6783bc3c9e37d40a83737b33ba7143d95a95ef78
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-08-18 16:13:22 +0300

    tried changing the order in yml file

--- Scripts/database_test.py
+++ Scripts/database_test.py
@@ -31,7 +31,7 @@ except socket.error as e:
 """
 # Connect to the MySQL container
 connection = mysql.connector.connect(
-    host="mysqldb",
+    host=os.getenv("DB_HOST"),
     port=os.getenv("DB_PORT"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
