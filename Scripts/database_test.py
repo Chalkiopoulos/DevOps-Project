a7be6940e7fa0cd1407741530ab9942ba8acf76a
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-08-19 00:38:46 +0300

    tidy workflows with a new custom action that both caches and installs dependecies

--- Scripts/database_test.py
+++ Scripts/database_test.py
@@ -54,7 +54,7 @@ for row in cursor.fetchall():
     print(row[1]) # only want the the second element of the tuple that fetchall() returns
     cyphertext=encrypt(row[1])  # Encrypt the plaintext value
 
-    print(cyphertext)
+   # print(cyphertext)
     cursor.execute(
         "INSERT INTO encryptions (plaintext_id, key_value, encryption_value) VALUES (%s, %s, %s)",
         (row[0], enc_key, cyphertext)
