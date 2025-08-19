5acab209f27834d8dfd7dcb6aab0a5f7e3883853
Nick Chalkiopoulos <58607213+UnburdenSoul@users.noreply.github.com>
2025-08-19 19:15:25 +0300

    Added automated deplaoyment of the app as docker image to dockerhub

--- Scripts/main.py
+++ Scripts/main.py
@@ -12,15 +12,19 @@ with open("testing_passwords.txt", "r") as f:       #reads the passwords
 
 
 for password in passwords:
+    print(f"Processing password: {password}")
     md5_hash = hash_password(password, "md5")
+    print(f"MD5: {md5_hash}")
     sha256_hash = hash_password(password, "sha256")
+    print(f"SHA256: {sha256_hash}")
     bcrypt_hash = hash_password(password, "bcrypt")
+    print(f"Bcrypt: {bcrypt_hash}")
     with open("hashed_passwords.txt", "a") as f:  #appends the hashes to a file
         f.write(f"Password: {password}\n")
         f.write(f"MD5: {md5_hash}\n")
         f.write(f"SHA256: {sha256_hash}\n")
         f.write(f"Bcrypt: {bcrypt_hash}\n\n")
-        
+
 # this plays with the pyfiglet lib
 ascii_banner = pyfiglet.figlet_format("GitHub actions are COOL")
 print(ascii_banner)
